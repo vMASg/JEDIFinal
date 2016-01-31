4af53597af0cdd6cb7380b15c7d878238937107a
Víctor Mas <victor.mas@est.fib.upc.edu>
2016-01-31 18:51:49 +0100

    Initial login feature commit

---
+++ app/src/main/java/com/example/victor/jedifinal/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.victor.jedifinal.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.victor.jedifinal.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
