9ba713adf3acafe94dde3de319d67e9edb414395
Víctor Mas <victor.mas@est.fib.upc.edu>
2016-02-03 15:56:57 +0100

    Created basic elements

---
+++ app/src/main/java/com/example/victor/jedifinal/profile/ProfileContract.java
@@ -0,0 +1,15 @@
+package com.example.victor.jedifinal.profile;
+
+/**
+ * Created by inlab on 03/02/2016.
+ */
+public interface ProfileContract {
+
+    interface View {
+
+    }
+
+    interface Presenter {
+
+    }
+}
---
+++ app/src/main/java/com/example/victor/jedifinal/profile/ProfilePresenter.java
@@ -0,0 +1,7 @@
+package com.example.victor.jedifinal.profile;
+
+/**
+ * Created by inlab on 03/02/2016.
+ */
+public class ProfilePresenter implements ProfileContract.Presenter {
+}
