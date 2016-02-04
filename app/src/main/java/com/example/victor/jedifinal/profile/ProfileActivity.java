d3ae3cb7578a18f0d7eea78236132b2082b29417
Víctor Mas <victor.mas@est.fib.upc.edu>
2016-02-04 17:08:01 +0100

    Added Profile picture

--- app/src/main/java/com/example/victor/jedifinal/profile/ProfileActivity.java
+++ app/src/main/java/com/example/victor/jedifinal/profile/ProfileActivity.java
@@ -3,6 +3,7 @@ package com.example.victor.jedifinal.profile;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.victor.jedifinal.Injector;
@@ -11,7 +12,8 @@ import com.example.victor.jedifinal.R;
 public class ProfileActivity extends AppCompatActivity implements ProfileContract.View {
 
     private ProfileContract.Presenter presenter;
-    private TextView username_tv;
+    private TextView usernameTv;
+    private ImageView profilePicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +23,8 @@ public class ProfileActivity extends AppCompatActivity implements ProfileContrac
         Injector.context = getApplicationContext();
         presenter = Injector.getProfilePresenter(this);
 
-        username_tv = (TextView) findViewById(R.id.profile_tw_username);
+        usernameTv = (TextView) findViewById(R.id.profile_tw_username);
+        profilePicture = (ImageView) findViewById(R.id.profile_imageView);
 
         SharedPreferences settings = getSharedPreferences("userActive", 0);
         presenter.setCurrentUser(settings.getString("userName", ""));
@@ -29,7 +32,7 @@ public class ProfileActivity extends AppCompatActivity implements ProfileContrac
 
     @Override
     public void showUserName(String username) {
-         username_tv.setText(username);
+         usernameTv.setText(username);
     }
 
     @Override
@@ -40,5 +43,7 @@ public class ProfileActivity extends AppCompatActivity implements ProfileContrac
     @Override
     public void showDefaultProfileImage() {
 //        TODO: implement method
+        profilePicture.setImageResource(R.drawable.default_image);
+
     }
 }
