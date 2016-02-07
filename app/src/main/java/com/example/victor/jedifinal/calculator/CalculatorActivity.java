d1e83c5114ef43bdc33e2d83388f6999b51acd1a
Víctor Mas <victor.mas@est.fib.upc.edu>
2016-02-07 20:02:16 +0100

    Calculator initial commit, created files

---
+++ app/src/main/java/com/example/victor/jedifinal/calculator/CalculatorActivity.java
@@ -0,0 +1,15 @@
+package com.example.victor.jedifinal.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.victor.jedifinal.R;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+}
